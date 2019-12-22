d83a984cbe858ec2c2ed18288b6abebbad4193bb
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2019-12-22 04:02:14 +0100

    Updates

---
+++ precache-manifest.3095f557d7ebc8644d8858c42b1fb829.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c14a611e7f3caa819f7a86dd12553d2",
+    "url": "/treasure-hunt2.0/index.html"
+  },
+  {
+    "revision": "26b1ceddc40d017a6421",
+    "url": "/treasure-hunt2.0/static/css/main.56f11da4.chunk.css"
+  },
+  {
+    "revision": "1be5b1c4f874dec7ce37",
+    "url": "/treasure-hunt2.0/static/js/2.c2c30c20.chunk.js"
+  },
+  {
+    "revision": "26b1ceddc40d017a6421",
+    "url": "/treasure-hunt2.0/static/js/main.5d106844.chunk.js"
+  },
+  {
+    "revision": "3ce666c35bcf9eca7457",
+    "url": "/treasure-hunt2.0/static/js/runtime-main.88d44252.js"
+  },
+  {
+    "revision": "5bd9527074f0ceba99edb58376ee6e51",
+    "url": "/treasure-hunt2.0/static/media/Hit.5bd95270.mov"
+  },
+  {
+    "revision": "6d63fdd38e18d18ad00b3eb78fe221eb",
+    "url": "/treasure-hunt2.0/static/media/Miss.6d63fdd3.wav"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/treasure-hunt2.0/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "8214ceca24cd7fdad0d4df27891f05ae",
+    "url": "/treasure-hunt2.0/static/media/tile.8214ceca.jpg"
+  }
+]);
